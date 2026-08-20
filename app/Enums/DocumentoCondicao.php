83a6b8f9a8af8d1d67d098cd69bf6965b45c99c3
Lucas Canestraro <lucascanestrarooliveira@gmail.com>
2026-08-19 22:13:43 -0300

    Enums apagadas (de novo). Esqueci de apagar antes de commitar da última vez

--- app/Enums/DocumentoCondicao.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace App\Enums;
-
-enum DocumentoCondicao: string
-{
-    case Acpp = 'acpp';
-    case Pae = 'pae';
-}
--- app/Enums/UserStatus.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace App\Enums;
-
-enum UserStatus: string
-{
-    case Pendente = 'pendente';
-    case Ativo = 'ativo';
-}
